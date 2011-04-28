5ae650f827ce861b23072f9284fad996b8b18132
Hunter 'abzde' Carroll <abzde@abzde.com>
2011-04-28 00:18:04 -0400

    I failed at merging.. broke runserver.py. it's back now!

---
+++ runserver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.2
+
+import sys
+import logging
+
+log = logging.getLogger('main')
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+log.debug('main')
+
+sys.path.append('../')
+from stirling.lib.daemon.server.server import runserver
+
+try:
+    runserver()
+except SystemExit:
+    exit()
+except:
+    log.debug('exception!', exc_info=sys.exc_info())
+    pass
