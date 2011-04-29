334524b7f25c7927a1251bfa4f23236899352051
Jonathan Howard <jonathan@Howard-BigiMac.local>
2011-04-29 03:04:08 -0400

    - Changed self.exits to a dictionary - Added the functions set_exits(), add_exits(), remove_exit(), and exits() functions

--- stirling/obj/room.py
+++ stirling/obj/room.py
@@ -12,7 +12,7 @@ class Room(MasterObject):
         super(Room, self).__init__()
         self.rm_nametag('object')
         self.lookables = {}
-        self.exits = []
+        self.exits = {}
     def write(self, data):
         """
         write(text)
@@ -22,3 +22,21 @@ class Room(MasterObject):
         # Need to add a link to the microsyntax interpretter when we have it.
         for item in self.inventory:
             item.tell(data+'\n')
+    	#Insert a control structure to place the exits within this text.
+    
+    #Exit functions
+    def set_exits(exit_dict):
+    	self.exits = exit_dict
+    def add_exit(dir, obj, hidden=False):
+    	self.exits[dir] = (obj, hidden)
+    def remove_exit(dir):
+    	del exit[dir]
+    def exits(visible=True):
+    	results_array = []
+    	if visible == True: #return only the visible exits
+    		for dir in self.exits: #Iterate through the exits
+    			if self.exits[dir][1] == False: #Selects the visible exits
+    				results_array.append(dir)
+    		return results_array #Hopefully this indentation is correct
+    	else: #This allows for the system and creators to see all exits
+    		return keys(self.exits)
