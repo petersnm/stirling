bde73d0d3e3847a125f57df9f8c047558407ea5b
Benjamin Allen <ploosk@turbogoat.net>
2011-05-04 22:07:20 +0800

    bettar?!

--- world/b/alot_of_hoverbears.py
+++ world/b/alot_of_hoverbears.py
@@ -11,6 +11,6 @@ from stirling.obj.living.npc import NPC
 class Alot(NPC):
     def __init__(self):
         super(Alot, self).__init__()
-        self.set_name('alot of hoverbears')
-        self.set_desc('This large, floating ball is made entirely of '
-          'hoverbears. It produces a low humming noise as it levitates.')
+        self.set_name = 'alot of hoverbears'
+        self.set_desc = 'This large, floating ball is made entirely of '
+          'hoverbears. It produces a low humming noise as it levitates.'
