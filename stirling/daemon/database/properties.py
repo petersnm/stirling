249251beeb3a6ef8b45eb44f1b95bcaf97a6c006
Hunter 'abzde' Carroll <abzde@abzde.com>
2011-05-06 10:08:01 -0400

    fixed the moose thing. partially.

--- stirling/daemon/database/properties.py
+++ stirling/daemon/database/properties.py
@@ -1,34 +1,37 @@
 from stirling.daemon.database import MongoDB
+import sys
+import logging
+logging.getLogger('cocks')
 
 objects = {}
 
 class Properties(dict):
     def __init__(self, parent, from_dict={}, from_db=False):
         dict.__init__(self, from_dict)
-        self.parent = parent
         # this is becasue if it's from the DB, we dont' need to fill the db, but if it isn't, we do.
         if not from_db:
             self['_id'] = MongoDB.objects.insert(self)
-            self.parent.__dict__['_id'] = self['_id']
-            self['_class'] = self.parent.__class__.__name__
-            self['_module'] = self.parent.__class__.__module__
-            objects[self['_id']] = self.parent
+            parent.__dict__['_id'] = self['_id']
+            self['_class'] = parent.__class__.__name__
+            self['_module'] = parent.__class__.__module__
+            objects[self['_id']] = parent
 
     
     def __setitem__(self, item, value):
+        wat = dict.__setitem__(self, item, value)
         try:
-            self.parent.debug('item set '+item+":"+value)
-        except:
-            pass
-        dict.__setitem__(self, item, value)
-        MongoDB.objects.save(self)
+            MongoDB.objects.save(self)
+        except Exception:
+            logging.debug(sys.exc_info())
+        return wat
 
     def __getitem__(self, item):
         return dict.__getitem__(self, item)
 
     def __delitem__(self, item):
-        dict.__delitem__(item)
+        wat = dict.__delitem__(item)
         MongoDB.objects.save(self)
+        return wat
 
     def save(self):
         MongoDB.objects.save(self)
