31d188ff782292861e507fd06cb92a46f4dfb1d9
Morgan Sennhauser <emsenn@emsenn.com>
2011-05-06 07:19:48 -0400

    Added glubbing alot for issue #47

---
+++ world/b/glubbing_alot.py
@@ -0,0 +1,8 @@
+import stirling
+from stirling.obj.living import Living
+
+class Alot(Living, **kw):
+    def __init__():
+        super(Alot, self).__init__(**kw)
+        self.name = "glubbing alot"
+        self.desc = 'This alot is glubbing around the area quite contently.'
