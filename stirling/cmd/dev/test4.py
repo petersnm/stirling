6856f2fee6ba567a3ecea50bbce10cbe14a2998e
Hunter 'abzde' Carroll <abzde@abzde.com>
2011-06-08 21:12:38 -0400

    bugfixes, clean up done on shutdown/disconnect

--- stirling/cmd/dev/test4.py
+++ stirling/cmd/dev/test4.py
@@ -4,6 +4,7 @@ def do_test4(origin, *a, **kw):
     foo = origin.environment.inventory
     for item in foo:
         i = stirling.get(item)
+        print(i)
         if type(i) != dict:
             if i.name == 'toothpick':
                 i.destroy()
