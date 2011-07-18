f74f5b751897074a3da5a58133036266742a0bc2
Morgan Sennhauser <emsenn@emsenn.com>
2011-07-18 10:47:55 -0400

    Added another alot

---
+++ world/b/alot_easier.py
@@ -0,0 +1,10 @@
+from stirling.obj.living.npc import NPC
+
+class Alot(NPC):
+    ''' https://plus.google.com/113208553012122994072/posts/e6HXXCPA8vG '''
+    def __init__(self):
+        super(Alot, self).__init__()
+        self.set_name('alot easier')
+        self.set_desc('This alot enjoys making tasks more simple for '
+          'you.  Always ready with a helping paw, he or she simply adores '
+          'making things alot easier.')
