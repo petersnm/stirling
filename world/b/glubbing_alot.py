14eb0eaff961688f13ebedb57e2be6531a70eead
Morgan Sennhauser <emsenn@emsenn.com>
2011-07-18 13:34:53 -0400

    Removing unnecessary files

--- world/b/glubbing_alot.py
+++
@@ -1,8 +0,0 @@
-import stirling
-from stirling.obj.living import Living
-
-class Alot(Living, **kw):
-    def __init__():
-        super(Alot, self).__init__(**kw)
-        self.name = "glubbing alot"
-        self.desc = 'This alot is glubbing around the area quite contently.'
