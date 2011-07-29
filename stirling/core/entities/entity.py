aa3cdd4185fa76718e8153bb2cd4381fc167d4cc
Percussor <thepragprog@gmail.com>
2011-07-29 11:46:33 -0400

    First working draft of the commit done new changes can be pulled down.

--- stirling/core/entities/entity.py
+++ stirling/core/entities/entity.py
@@ -1,10 +1,15 @@
-import stirling
+mport stirling
 from stirling.core.daemons import MongoDB
 
 class Entity(stirling.core.BaseObj):
     """
+        .. py:class:: Entity()
+            :versionadded: 0.1.0
     """
     def __init__(self, from_dict={}, from_db=False, **kw):
+        """ This is the inheritable class that defines every object in the engine.                        
+        
+        """
         super(Entity, self).__init__(**kw)
         self.__dict__['exclude'] = ['properties', 'logger', 'debug', 'info',
           'warning', 'error', 'save', 'move', 'remove', 'tell',
@@ -18,6 +23,13 @@ class Entity(stirling.core.BaseObj):
         self.metrics    = {}
 
     def __setattr__(self, attr, value):
+        """ Sets an attribute in the current class.
+            :param attr: the attribute in question that you wish to change.
+                :type string:
+            :param value: the value you wish to set the attribubte to.
+           
+            ``__baseattr__`` is the base setter for every entity in the engine. The function makes checks for custom getters before doing anything. If one is found, the custom getter is called. If no custom setter is found,  the value is saved in self.__dict__['properties'][attr] unless the attribute *already exists* as self.attribute.
+        """
         if "_set_%s" % (attr,) in dir(self):
             return object.__getattribute__(self, "_set_%s" % (attr,))(value)
         elif attr in self.exclude or attr in self.__dict__:
@@ -38,6 +50,10 @@ class Entity(stirling.core.BaseObj):
                     return self.__dict__['properties'][attr]
 
     def __delattr__(self, attr):
+        """
+            :warning: Deleting an attribute that is in ``self.exclude`` is ***dangerous***,\
+            as it breaks core functionality and the way the engine talks to the database.
+        """
         if hasattr(self, "_del_%s" % (attr,)) and not attr.startswith('_del_'):
             return getattr(self, "_del_%s" % (attr,))
         elif attr in self.exclude:
@@ -47,12 +63,21 @@ class Entity(stirling.core.BaseObj):
             del self.properties[attr]
 
     def move(self, destination):
+        """
+            :todo: Write the actual move() function.
+        """
         pass
 
     def remove(self):
+        """
+            :todo: Write the actual remove() function.
+        """
         pass
 
     def destroy(self):
+        """
+            :todo: Write the actual destroy() function.
+        """
         pass
 
 
