3181d31e525340bf07ea608a7a679e40f918e7de
Morgan Sennhauser <emsenn@emsenn.com>
2012-03-10 13:54:52 -0500

    Update to /stirling/daemons/servers/http/__init__.py, added some documentation.

--- daemons/servers/http/__init__.py
+++ daemons/servers/http/__init__.py
@@ -15,7 +15,28 @@ from stirling.daemons.servers.http.default_app import default
 logger = logging.getLogger(__name__)
 
 class HTTPServer(threading.Thread):
+    """ HTTPServer() is a class defined to set up and run an HTTP server.  
+        Stirling's HTTP server exists to create and serve pages offering 
+        data about the engine's operations, while currently not providing 
+        any interactive methods.
+    """
     def __init__(self, addr, *a, **kw):
+        """ Initialize the HTTP server.
+        
+            :param  addr:       A tuple of (``Host``, ``Port``)
+            :type   addr:       tuple
+            
+            :param  \*a:        Catch-all for HTTPServer's parent's __init__
+            :type   \*a:        mixed
+            
+            :param \*\*kw:      Additional keywords are passed to HTTPServer's
+                                parent, in this case :py::class:`threading.Thread`.
+            :type  \*\*kw:      mixed
+            
+            :returns:           None
+            
+            Creates a new WSGI (Web Server Gateway Interface) server bound to `addr`.
+        """
         super(HTTPServer, self).__init__(*a, **kw)
         self.addr = addr
         self.logger = logger
@@ -23,4 +44,8 @@ class HTTPServer(threading.Thread):
         self.server.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def run(self):
+        """ HTTPServer.run() well, runs the HTTP server.
+        
+            :returns:           None
+        """
         self.server.serve_forever()
